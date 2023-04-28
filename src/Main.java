26908fdc0239236f3b29f06844e6c59fe2573f8a
amorags <115067025+amorags@users.noreply.github.com>
2023-04-28 10:43:08 +0200

    Login Opens

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+        public static void main(String[] args){ launch(args);
+        }
+
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            //Parent root = FXMLLoader.load(getClass().getResource("TicketSystem/GUI/View/EventMaker.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("GUI/View/LogInView.fxml"));
+            primaryStage.setTitle("LogIn");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setResizable(true);
+            primaryStage.show();
+        }
+
     }
-}
