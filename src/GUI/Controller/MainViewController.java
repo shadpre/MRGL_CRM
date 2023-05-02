7bda760743edf32a2d38e04119597e7b9679f4c5
JSM80 <mail@moellers-hule.dk>
2023-05-02 09:53:05 +0200

    jesper mainView

---
+++ src/GUI/Controller/MainViewController.java
@@ -0,0 +1,95 @@
+package GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+
+public class MainViewController {
+    @FXML
+    private Button btnAddUser;
+
+    @FXML
+    private Button btnCreateOrder;
+
+    @FXML
+    private Button btnDeleteUser;
+
+    @FXML
+    private Button btnShowAllOrders;
+
+    @FXML
+    private Button btnShowAllUsers;
+
+    @FXML
+    private Button btnShowCurrent;
+
+    @FXML
+    private Button btnShowFinished;
+
+    @FXML
+    private Button btnShowUpcoming;
+
+    @FXML
+    private Button btnUpdateOrder;
+
+    @FXML
+    private Button btnUpdateUser;
+
+    @FXML
+    private StackPane stackpane;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    void btnHandleAddUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHandleCreateOrder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHandleDeleteUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHandleShowAllOrders(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHandleShowAllUsers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHandleShowCurrent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHandleShowFinished(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHandleShowUpcoming(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHandleUpdateOrder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHandleUpdateUser(ActionEvent event) {
+
+    }
+}
