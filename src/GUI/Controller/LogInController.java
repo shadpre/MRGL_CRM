a1821c3d5ded96d29b8fd76f904aab01f21f90be
amorags <115067025+amorags@users.noreply.github.com>
2023-05-04 19:38:09 +0200

    open mainView after login.

--- src/GUI/Controller/LogInController.java
+++ src/GUI/Controller/LogInController.java
@@ -1,12 +1,17 @@
 package GUI.Controller;
 
+import BE.Exptions.UserValidationExeption;
 import BE.User;
 import GUI.Model.UserModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -41,7 +46,18 @@ public class LogInController extends BaseController implements Initializable {
         userModel.setSelectedUser(selectedUser);
 
         if (selectedUser != null){
-            System.out.println(selectedUser.toString());
+            userModel.setSelectedUser(selectedUser);
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/MainView.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle(selectedUser.getLoginName());
+            stage.show();
+        }
+
+        else{
+            throw new UserValidationExeption("Invalid Username or password");
         }
     }
 }
