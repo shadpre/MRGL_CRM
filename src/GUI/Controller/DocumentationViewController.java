4d3a0ada87a359c3388500878065f773dc40c72e
amorags <115067025+amorags@users.noreply.github.com>
2023-05-08 12:35:47 +0200

    TableViewUpdate

--- src/GUI/Controller/DocumentationViewController.java
+++ src/GUI/Controller/DocumentationViewController.java
@@ -1,10 +1,13 @@
 package GUI.Controller;
 
+import BE.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -24,6 +27,15 @@ public class DocumentationViewController extends BaseController implements Initi
 
     @FXML
     private Button btnAddSketch;
+
+    @FXML
+    private TableView<User> tbwAllUsers;
+    @FXML
+    private TableColumn<User, String> columnLastName;
+    @FXML
+    private TableColumn<User, String> columnFirstName;
+    @FXML
+    private TableColumn<User, String> columnEmail;
 
     @FXML
     private Button btnExit;
