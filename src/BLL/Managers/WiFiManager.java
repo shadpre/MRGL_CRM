9bde5361929cd1468a995a78a039d493c982ed2b
shadpre <klavs@tranborg.eu>
2023-05-15 12:49:23 +0200

    Refactor

--- src/BLL/Managers/WiFiManager.java
+++ src/BLL/Managers/WiFiManager.java
@@ -1,7 +1,18 @@
 package BLL.Managers;
 
 import BE.DBEnteties.WiFi;
+import DAL.DB.WiFiDAO_DB;
 
 public class WiFiManager {
     WiFi wifi;
-}
+
+
+    public void init(){
+    try
+    {
+        var test = WiFiDAO_DB.getWiFis(1);
+    }
+    catch(Exception ex){
+        System.out.println(ex.getMessage());
+    }
+}}
